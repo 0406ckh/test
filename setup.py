05ba07bad6fe537f596fc10107ffec1d4fa6394f
0406ckh <91605081+0406ckh@users.noreply.github.com>
2022-05-04 10:52:26 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data=True,
+  name='testcalc',
+  version='0.0.1',
+  description='oss-dev class test calculator',
+  author='0406ckh',
+  author_email='0406ckh@naver.com',
+  url="",
+  download_url="",
+  install_requires=['pytest'],
+  long_description='oss-dev calculator python module',
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operation System :: OS Independent",
+  ],
+)
+  
