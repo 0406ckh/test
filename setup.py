4bd70c74a0575b9d07c505cab8b06a9aa111550f
0406ckh <91605081+0406ckh@users.noreply.github.com>
2022-05-04 10:54:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
   author='0406ckh',
   author_email='0406ckh@naver.com',
   url="",
-  download_url="",
+  download_url="https://github.com/0406ckh/test/archive/refs/heads/search.zip",
   install_requires=['pytest'],
   long_description='oss-dev calculator python module',
   long_description_content_type='text/markdown',
